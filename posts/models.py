653bb61a8292e9545d6c8e9063dc654f8f740f8d
Samat-Omurzakov <samat-omurzakov@mail.ru>
2023-03-23 19:10:01 +0600

    1

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Post(models.Model):
+    image = models.ImageField(blank=True, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    rating = models.FloatField(default=0.0)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
